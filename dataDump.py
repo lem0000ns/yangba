d577e6d9f696a4adc05fb042d88849186a609555
lem0000ns <kevinyang.poke@gmail.com>
2024-07-13 10:39:05 -0700

    distributed time intervals for multithreading

--- dataDump.py
+++ dataDump.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import threading
+import random
 import time
 from main import getData, getSeasonStats
 
@@ -28,7 +29,7 @@ def test_time(func):
 def getAllPlayers(szn, playerStats, startTeamID):
     nbaTeamIds = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 38, 40, 41]
     #nbaTeamIds = [29, 30, 31, 38, 40, 41]
-    for team in range(1, 31):
+    for team in range(startTeamID, startTeamID + 15):
         teamID = nbaTeamIds[team - 1]
         teamName = getData(f"/teams?id={teamID}")['response'][0]['name'].replace(" ", "")
         teamPlayers = getData(f"/players?season={szn}&team={teamID}")
@@ -42,6 +43,7 @@ def getAllPlayers(szn, playerStats, startTeamID):
                 playerStats[name] = sznStats
             else:
                 print(f'Failed to fetch player {name} data for season {szn}')
+        time.sleep(5 * random.random())
         dumpS3(playerStats, teamName, szn)
         playerStats = {}
 
@@ -49,21 +51,15 @@ def getAllPlayers(szn, playerStats, startTeamID):
 def startDump():
     try:
         playerStats = {}
-        # only for multiple seasons
-        # seasons = getData("/seasons")['response'][-1]
-        # threads = []
-        # for szn in seasons:
-        #     print(szn)
-        #     #running 2 threads concurrently for 30 teams (each thread = 15 teams)
-        #     for i in range(1):
-        #         thread = threading.Thread(target=getAllPlayers, args=(szn, playerStats, 1 + 30 * i))
-        #         threads.append(thread)
-        #         thread.start()
-        #     for thread in threads:
-        #         thread.join()
-        #     threads = []
+        threads = []
         lastSeason = getData("/seasons")['response'][-1]
-        getAllPlayers(lastSeason, playerStats, 1)
+        #running 2 threads concurrently for 30 teams (each thread = 15 teams)
+        for i in range(2):
+            thread = threading.Thread(target=getAllPlayers, args=(lastSeason, playerStats, 1 + 15 * i))
+            threads.append(thread)
+            thread.start()
+        for thread in threads:
+            thread.join()
         
     except Exception as e:
         print(f"An error occurred: {str(e)}")
