ca4bff9d8e2672b5cf6bf4716149f239c8d51506
lem0000ns <kevinyang.poke@gmail.com>
2024-08-04 01:22:14 -0700

    restructured code into classes in separate file

--- dataDump.py
+++ dataDump.py
@@ -1,13 +1,9 @@
-import json, boto3, threading, time, requests, pymysql, math, traceback
-from random import randint
-from datetime import datetime, timedelta
-import boto3
+import json, boto3, requests, pymysql
+from utilities import MySQL_Writer, DataDumper
 
 def get_secret():
     secret_name = "Mysql/password"
     region_name = "us-west-1"
-
-    # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
@@ -24,8 +20,6 @@ def get_secret():
         raise
 
 #configuration variables
-bucket_name = "nba-players.bucket"
-s3_client = boto3.client('s3', 'us-west-1')
 url = "https://api-nba-v1.p.rapidapi.com"
 headers = {
     'x-rapidapi-key': "b990592d51msh5e1029396589d1bp18dd72jsnce5f8c3e8b6c",
@@ -38,16 +32,6 @@ connection = pymysql.connect(
     password=pw, 
     database='yba'
 )
-cursor = connection.cursor()
-allNames = set()
-
-def test_time(func):
-    def wrapper(*args, **kwargs):
-        start_time = int(time.time())
-        res = func(*args, **kwargs)
-        print(f"{func} took {int(time.time()) - start_time} seconds")
-        return res
-    return wrapper
 
 def dumpJson(data, testFile):
     if not isinstance(testFile, str):
@@ -61,15 +45,6 @@ def dumpJson(data, testFile):
         print(f"Could not dump into json file with exception {e}")
         return False
 
-#dumps into s3 bucket in ndjson format
-def dumpS3(data, s3_path):
-    jsonl_string = '\n'.join(json.dumps(item) for item in data)
-    s3_client.put_object(
-        Key=s3_path,
-        Bucket=bucket_name,
-        Body=jsonl_string
-    )
-
 def getData(endpoint):
     try:
         response = requests.get(url + endpoint, headers=headers)
@@ -82,222 +57,24 @@ def getData(endpoint):
     print(f"Could not fetch data with endpoint {endpoint} with status code {response.status_code}")
     return None
 
-def getSeasonStats(lastSzn, stats):
-    try:
-        id = stats['id']
-        data = getData("/players/statistics?season={}&id={}".format(lastSzn, id))['response']
-        if data is not None:
-            return data
-        return None
-    except Exception as e:
-        print(f"Could not fetch season stats for this player")
-        return None
-
-@test_time
-#gets data in regular json format to be parsed into ndjson format by flattenDump using 2 threads
-def getAllPlayers(szn, data, startTeamID, onlyNames):
-    nbaTeamIds = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 38, 40, 41]
-    for team in range(startTeamID, startTeamID + 15):
-        teamID = nbaTeamIds[team - 1]
-        teamPlayers = getData(f"/players?season={szn}&team={teamID}")
-        print("Currently on team " + str(team))
-        for p in range(0, len(teamPlayers['response'])):
-            stats = teamPlayers['response'][p]
-            name = stats['firstname'].lower() + " " + stats['lastname'].lower()
-            #dumps names in names.txt
-            if onlyNames:
-                if name not in allNames:
-                    data.append(name)
-                    allNames.add(name)
-            #stores in hashmap with players' names as keys and their szn stats as values
-            else:
-                sznStats = getSeasonStats(szn, stats)
-                if (sznStats):
-                    data[name] = sznStats
-                else:
-                    print(f'Failed to fetch player {name} data for season {szn}')
-        # time.sleep(randint(1,5))
-
-#calculating OPI using box score stats and scaling it relative to how many min they played and how many total pts team scored
-def calculateOPI(playerStats, gameID):
-    cursor.execute(f" SELECT home, homePts, away, awayPts FROM games WHERE gameID = {gameID}")
-    result = cursor.fetchone()
-    if result is None or result[0] == "":
-        playerStats["OPI"] = None
-    else:
-        try:
-            if playerStats["team"] == result[0]:
-                totalPts = result[1]
-            else:
-                totalPts = result[3]
-            playerStats["OPI"] = (playerStats["points"] + 3 * playerStats["3pm"] - playerStats["3pa"] + playerStats["ftm"] - playerStats["fta"] + 2 * playerStats["fgm"] - playerStats["fga"] + 2 * playerStats["ast"] - playerStats["turnovers"])
-            minFactor = math.log10(playerStats["min"] + 10)
-            playerStats["OPI"] = abs(playerStats["OPI"] / float(minFactor * totalPts))
-        except Exception as e:
-            print(f"Failed to calculate OPI with exception: {e}")
-        
-@test_time
-def flattenDump(season):
-    try:
-        #reading from s3 to get player data
-        obj = s3_client.get_object(Bucket=bucket_name, Key=f"version1/allPlayers{season}.json")
-        json_data = obj['Body'].read().decode('utf-8')
-        data = json.loads(json_data)
-
-        playerStats = {}
-        allStats = []
-
-        for player in data:
-            for currGame in range(len(data[player])):
-                currPlayer = data[player][currGame]
-                #filters out players who didn't play / registered 0 minutes
-                valid = (
-                    currPlayer["min"] not in ["--", "-", "0", "0:00", "00:00"]
-                    and currPlayer["min"] is not None
-                    and currPlayer["points"] is not None
-                )
-                if valid:
-                    playerStats["name"] = currPlayer["player"]["firstname"] + " " + currPlayer["player"]["lastname"]
-                    playerStats["playerID"] = currPlayer["player"]["id"]
-                    gameID = currPlayer["game"]["id"]
-                    playerStats["gameID"] = gameID
-                    playerStats["team"] = currPlayer["team"]["code"]
-                    playerStats["season"] = season
-                    playerStats["points"] = currPlayer["points"]
-                    #if min has : in it e.g. 30:35
-                    index = currPlayer["min"].find(':')
-                    playerStats["min"] = (int)(currPlayer["min"][:index]) if index != -1 else (int)(currPlayer["min"])
-                    playerStats["fgm"] = currPlayer["fgm"]
-                    playerStats["fga"] = currPlayer["fga"]
-                    playerStats["ftm"] = currPlayer["ftm"]
-                    playerStats["fta"] = currPlayer["fta"]
-                    playerStats["3pm"] = currPlayer["tpm"]
-                    playerStats["3pa"] = currPlayer["tpa"]
-                    playerStats["reb"] = currPlayer["totReb"]
-                    playerStats["ast"] = currPlayer["assists"]
-                    playerStats["steals"] = currPlayer["steals"]
-                    playerStats["blocks"] = currPlayer["blocks"]
-                    playerStats["turnovers"] = currPlayer["turnovers"]
-                    calculateOPI(playerStats, gameID)
-                    allStats.append(playerStats)
-                    playerStats = {}
-                        
-    except Exception as e:
-        print(f"Error flattening data with exception: {e}")
-        traceback.print_exc()
-    
-    dumpS3(allStats, f"version2/playerStats{season}.json")
-
-@test_time
-#puts game stages in s3 bucket
-def getGameStages(season):
-    allGames = []
-    allGames = []
-    data = getData(f"/games?season={season}")['response']
-    for game in data:
-        currGame = {}
-        if (game["league"] == "standard"):
-            currGame['gameID'] = game['id']
-            currGame['stage'] = game['stage']
-            gameTime = game['date']['start']
-            if len(gameTime) > 10:
-                utc_time = datetime.strptime(gameTime, '%Y-%m-%dT%H:%M:%S.%fZ')
-                pdt_time = utc_time - timedelta(hours=7)
-            else:
-                utc_time = datetime.strptime(gameTime, '%Y-%m-%d')
-                pdt_time = utc_time - timedelta(days=1)
-            currGame['gameDate'] = pdt_time.strftime('%m-%d-%Y')
-            currGame['home'] = game['teams']['home']['code']
-            currGame['homePts'] = game['scores']['home']['points']
-            currGame['away'] = game['teams']['visitors']['code']
-            currGame['awayPts'] = game['scores']['visitors']['points']
-            allGames.append(currGame)
-    dumpS3(allGames, f"gameStages/games{season}.json")
-
-@test_time
-def writeMySQL(table, season):
-    try:
-        cursor = connection.cursor()
-        if table == "version2":
-            columns = ["name", "playerID", "gameID", "season", "points", "min", "fgm", "fga", "ftm", "fta", "3pm", "3pa", "reb", "ast", "steals", "blocks", "turnovers", "OPI"]
-            key_template = "version2/playerStats%s.json"
-
-        elif table == "games":
-            columns = ["gameID", "stage", "gameDate", "home", "homePts", "away", "awayPts"]
-            key_template = "gameStages/games%s.json"
-
-        columns_placeholder = ', '.join(columns)
-        values_placeholder = ', '.join(['%s'] * len(columns))
-
-        # get actual data from S3 bucket and transfer to MySQL
-        key = key_template % season
-        obj = s3_client.get_object(Bucket=bucket_name, Key=key)
-        ndjson_data = obj['Body'].read().decode('utf-8')
-        data = [json.loads(line) for line in ndjson_data.splitlines()]
-        
-        for game in data:
-            values = [game.get(col, None) for col in columns]
-            insert_query = f"INSERT INTO {table} ({columns_placeholder}) VALUES ({values_placeholder}) ON DUPLICATE KEY UPDATE "
-            update_clause = ', '.join([f"{col} = VALUES({col})" for col in columns])
-            insert_query += update_clause
-            #ignore player games who played in summer league / another league outside of nba
-            try:
-                cursor.execute(insert_query, values)
-            except Exception as e:
-                continue
-        
-        #normalize OPI values using MySQL
-        if table == "version2":
-            cursor.execute(f"SELECT MAX(OPI) FROM version2 WHERE season={season}")
-            max_OPI = cursor.fetchone()[0]
-            cursor.execute(f"SELECT MIN(OPI) FROM version2 WHERE season={season}")
-            min_OPI = cursor.fetchone()[0]
-            cursor.execute(f"UPDATE version2 SET OPI = ROUND((OPI - {min_OPI}) / ({max_OPI} - {min_OPI}), 3) WHERE season={season}")
-        
-        connection.commit()
-    
-    except Exception as e:
-        print(f"Error connecting with MySQL with exception: {e}")
-
-@test_time
-def writeNames(season):
-    nameFile = s3_client.get_object(Bucket=bucket_name, Key="names.txt")
-    names = nameFile['Body'].read().decode('utf-8')
-    for n in names.splitlines():
-        allNames.add(n)
-
-    #adds any new names in most recent season to allNames
-    newNames = []
-    threads = []
-    for i in range(2):
-        thread = threading.Thread(target=getAllPlayers, args=(season, newNames, 1 + 15 * i, True))
-        threads.append(thread)
-        thread.start()
-    for thread in threads:
-        thread.join()
-        
-    for n in newNames:
-        allNames.add(n)
-        
-    s3_client.put_object(Bucket=bucket_name, Key="names.txt", Body="\n".join(allNames), ContentType='text/plain')
-
 def lambda_handler(event, context):
     try:
         #gets most recent season
         season = getData("/seasons")['response'][-1]
-        
-        #dumps all names into s3 txt file
-        writeNames(season)
-        
-        #maps gameids to stage/date in s3 as nba-players.bucket/gameStages/gamesXXXX.json
-        getGameStages(season)
-        
-        #gathers all player data from most recent season, puts in s3 as nba-players.bucket/version2/playerStatsXXXX.json
-        flattenDump(season)
+
+        dd = DataDumper("names", season)
+        dd.dumpData()
+        dd.setType("games")
+        dd.dumpData()
+        dd.setType("players")
+        dd.dumpData()
         
         #transfer data to MySQL, updating games and player data in version2 under most recent season
-        writeMySQL("games", season)
-        writeMySQL("version2", season)
+        sqlWriter = MySQL_Writer("games", connection, season)
+        sqlWriter.transferData()
+        sqlWriter.setTable("version2")
+        sqlWriter.transferData()
+        sqlWriter.normalizeOPI()
         
         if connection:
             connection.close()
@@ -315,17 +92,9 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     print("HI")
-    # season = 2019
-    # data = {}
-
-    # threads = []
-    # for i in range(2):
-    #     thread = threading.Thread(target=getAllPlayers, args=(season, data, 1 + 15 * i, False))
-    #     threads.append(thread)
-    #     thread.start()
-    #     time.sleep(5)
-    # for thread in threads:
-    #     thread.join()
-    
-    # json_data = json.dumps(data)
-    # s3_client.put_object(Bucket=bucket_name, Key=f"version1/allPlayers{season}.json", Body=json_data)
+    # test = DataDumper("names", 2023)
+    # test.dumpData()
+    # test.setType("games")
+    # test.dumpData()
+    # test.setType("players")
+    # test.dumpData()
---
+++ utilities.py
@@ -0,0 +1,242 @@
+import boto3, json, threading, traceback, math, time
+from datetime import datetime, timedelta
+from random import randint
+
+#configuration variables
+s3_client = boto3.client('s3', 'us-west-1')
+bucket_name = "nba-players.bucket"
+allNames = set()
+
+def test_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = int(time.time())
+        res = func(*args, **kwargs)
+        print(f"{func} took {int(time.time()) - start_time} seconds")
+        return res
+    return wrapper
+
+class MySQL_Writer:
+    def __init__(self, table, season):
+        self.table = table
+        self.season = season
+        if self.table == "version2":
+            self.columns = ["name", "playerID", "gameID", "season", "points", "min", "fgm", "fga", "ftm", "fta", "3pm", "3pa", "reb", "ast", "steals", "blocks", "turnovers", "OPI"]
+            self.key_template = "version2/playerStats%s.json"
+        elif self.table == "games":
+            self.columns = ["gameID", "stage", "gameDate", "home", "homePts", "away", "awayPts"]
+            self.key_template = "gameStages/games%s.json"
+        self.columns_placeholder = ', '.join(self.columns)
+        self.values_placeholder = ', '.join(['%s'] * len(self.columns))
+
+    @test_time
+    def transferData(self):
+        from dataDump import connection
+        cursor = connection.cursor()
+
+        key = self.key_template % self.season
+        obj = s3_client.get_object(Bucket=bucket_name, Key=key)
+        ndjson_data = obj['Body'].read().decode('utf-8')
+        data = [json.loads(line) for line in ndjson_data.splitlines()]
+        
+        for game in data:
+            values = [game.get(col, None) for col in self.olumns]
+            insert_query = f"INSERT INTO {self.table} ({self.columns_placeholder}) VALUES ({self.values_placeholder}) ON DUPLICATE KEY UPDATE "
+            update_clause = ', '.join([f"{col} = VALUES({col})" for col in self.columns])
+            insert_query += update_clause
+            #ignore player games who played in summer league / another league outside of nba
+            try:
+                cursor.execute(insert_query, values)
+            except Exception as e:
+                continue
+        
+        self.connection.commit()
+        
+    @test_time
+    def normalizeOPI(self):
+        from dataDump import connection
+        cursor = connection.cursor()
+        if self.table == "version2":
+            cursor.execute(f"SELECT MAX(OPI) FROM version2 WHERE season={self.season}")
+            max_OPI = cursor.fetchone()[0]
+            cursor.execute(f"SELECT MIN(OPI) FROM version2 WHERE season={self.season}")
+            min_OPI = cursor.fetchone()[0]
+            cursor.execute(f"UPDATE version2 SET OPI = ROUND((OPI - {min_OPI}) / ({max_OPI} - {min_OPI}), 3) WHERE season={self.season}")
+            self.connection.commit()
+
+    def setTable(self, newTable):
+        self.table = newTable
+
+class DataDumper:
+    def __init__(self, type, season):
+        self.type = type
+        self.season = season
+    
+    def setType(self, newType):
+        self.type = newType
+    
+    def setSeason(self, newSeason):
+        self.season = newSeason
+    
+    @test_time
+    def dumpData(self):
+        if self.type == "names":
+            nameFile = s3_client.get_object(Bucket=bucket_name, Key="names.txt")
+            names = nameFile['Body'].read().decode('utf-8')
+            for n in names.splitlines():
+                allNames.add(n)
+
+            #adds any new names in most recent season to allNames
+            threads = []
+            for i in range(2):
+                thread = threading.Thread(target=self.getAllPlayers, args=(self.season, allNames, 1 + 15 * i, True))
+                threads.append(thread)
+                thread.start()
+                time.sleep(5)
+            for thread in threads:
+                thread.join()
+                
+            s3_client.put_object(Bucket=bucket_name, Key="names.txt", Body="\n".join(allNames), ContentType='text/plain')
+        
+        elif self.type == "games":
+            allGames = []
+            from dataDump import getData
+            data = getData(f"/games?season={self.season}")['response']
+            for game in data:
+                currGame = {}
+                if (game["league"] == "standard"):
+                    currGame['gameID'] = game['id']
+                    currGame['stage'] = game['stage']
+                    gameTime = game['date']['start']
+                    if len(gameTime) > 10:
+                        utc_time = datetime.strptime(gameTime, '%Y-%m-%dT%H:%M:%S.%fZ')
+                        pdt_time = utc_time - timedelta(hours=7)
+                    else:
+                        utc_time = datetime.strptime(gameTime, '%Y-%m-%d')
+                        pdt_time = utc_time - timedelta(days=1)
+                    currGame['gameDate'] = pdt_time.strftime('%m-%d-%Y')
+                    currGame['home'] = game['teams']['home']['code']
+                    currGame['homePts'] = game['scores']['home']['points']
+                    currGame['away'] = game['teams']['visitors']['code']
+                    currGame['awayPts'] = game['scores']['visitors']['points']
+                    allGames.append(currGame)
+
+            self.dumpS3(allGames, f"gameStages/games{self.season}.json")
+        
+        elif self.type == "players":
+            try:
+                #reading from s3 to get player data
+                obj = s3_client.get_object(Bucket=bucket_name, Key=f"version1/allPlayers{self.season}.json")
+                json_data = obj['Body'].read().decode('utf-8')
+                data = json.loads(json_data)
+
+                playerStats = {}
+                allStats = []
+
+                for player in data:
+                    for currGame in range(len(data[player])):
+                        currPlayer = data[player][currGame]
+                        #filters out players who didn't play / registered 0 minutes
+                        valid = (
+                            currPlayer["min"] not in ["--", "-", "0", "0:00", "00:00"]
+                            and currPlayer["min"] is not None
+                            and currPlayer["points"] is not None
+                        )
+                        if valid:
+                            playerStats["name"] = currPlayer["player"]["firstname"] + " " + currPlayer["player"]["lastname"]
+                            playerStats["playerID"] = currPlayer["player"]["id"]
+                            gameID = currPlayer["game"]["id"]
+                            playerStats["gameID"] = gameID
+                            playerStats["team"] = currPlayer["team"]["code"]
+                            playerStats["season"] = self.season
+                            playerStats["points"] = currPlayer["points"]
+                            #if min has : in it e.g. 30:35
+                            index = currPlayer["min"].find(':')
+                            playerStats["min"] = (int)(currPlayer["min"][:index]) if index != -1 else (int)(currPlayer["min"])
+                            playerStats["fgm"] = currPlayer["fgm"]
+                            playerStats["fga"] = currPlayer["fga"]
+                            playerStats["ftm"] = currPlayer["ftm"]
+                            playerStats["fta"] = currPlayer["fta"]
+                            playerStats["3pm"] = currPlayer["tpm"]
+                            playerStats["3pa"] = currPlayer["tpa"]
+                            playerStats["reb"] = currPlayer["totReb"]
+                            playerStats["ast"] = currPlayer["assists"]
+                            playerStats["steals"] = currPlayer["steals"]
+                            playerStats["blocks"] = currPlayer["blocks"]
+                            playerStats["turnovers"] = currPlayer["turnovers"]
+                            self.calculateOPI(playerStats, gameID)
+                            allStats.append(playerStats)
+                            playerStats = {}
+                                
+            except Exception as e:
+                print(f"Error flattening data with exception: {e}")
+                traceback.print_exc()
+            
+            self.dumpS3(allStats, f"version2/playerStats{self.season}.json")
+
+    #calculating OPI using box score stats and scaling it relative to how many min they played and how many total pts team scored
+    def calculateOPI(self, playerStats, gameID):
+        from dataDump import connection
+        cursor = connection.cursor()
+
+        cursor.execute(f" SELECT home, homePts, away, awayPts FROM games WHERE gameID = {gameID}")
+        result = cursor.fetchone()
+        if result is None or result[0] == "":
+            playerStats["OPI"] = None
+        else:
+            try:
+                if playerStats["team"] == result[0]:
+                    totalPts = result[1]
+                else:
+                    totalPts = result[3]
+                playerStats["OPI"] = (playerStats["points"] + 3 * playerStats["3pm"] - playerStats["3pa"] + playerStats["ftm"] - playerStats["fta"] + 2 * playerStats["fgm"] - playerStats["fga"] + 2 * playerStats["ast"] - playerStats["turnovers"])
+                minFactor = math.log10(playerStats["min"] + 10)
+                playerStats["OPI"] = abs(playerStats["OPI"] / float(minFactor * totalPts))
+            except Exception as e:
+                print(f"Failed to calculate OPI with exception: {e}")
+
+    @test_time
+    #gets data in regular json format to be parsed into ndjson format by flattenDump using 2 threads
+    def getAllPlayers(self, szn, data, startTeamID, onlyNames):
+        from dataDump import getData
+        nbaTeamIds = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 38, 40, 41]
+        for team in range(startTeamID, startTeamID + 15):
+            teamID = nbaTeamIds[team - 1]
+            teamPlayers = getData(f"/players?season={szn}&team={teamID}")
+            print("Currently on team " + str(team))
+            for p in range(0, len(teamPlayers['response'])):
+                stats = teamPlayers['response'][p]
+                name = stats['firstname'].lower() + " " + stats['lastname'].lower()
+                #dumps names in names.txt
+                if onlyNames:
+                    if name not in allNames:
+                        allNames.add(name)
+                #stores in hashmap with players' names as keys and their szn stats as values
+                else:
+                    sznStats = self.getSeasonStats(szn, stats)
+                    if (sznStats):
+                        data[name] = sznStats
+                    else:
+                        print(f'Failed to fetch player {name} data for season {szn}')
+            time.sleep(randint(1,5))
+
+    def getSeasonStats(self, szn, stats):
+        from dataDump import getData
+        try:
+            id = stats['id']
+            data = getData("/players/statistics?season={}&id={}".format(szn, id))['response']
+            if data is not None:
+                return data
+            return None
+        except Exception as e:
+            print(f"Could not fetch season stats for this player")
+            return None
+        
+    #dumps into s3 bucket in ndjson format
+    def dumpS3(data, s3_path):
+        jsonl_string = '\n'.join(json.dumps(item) for item in data)
+        s3_client.put_object(
+            Key=s3_path,
+            Bucket=bucket_name,
+            Body=jsonl_string
+        )
+    
